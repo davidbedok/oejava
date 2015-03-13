d5e487c2db55a78ac8a0bbcdf27b538c3a562bf1
edvibed <david.bedok@ericsson.com>
2015-03-13 16:18:10 +0100

    ADD: log4j 2

---
+++ Bank/src/main/hu/qwaevisz/bank/core/Program.java
@@ -0,0 +1,14 @@
+package hu.qwaevisz.bank.core;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Program {
+
+	private static final Logger LOGGER = LogManager.getLogger(Program.class);
+
+	public static void main(final String[] args) {
+		LOGGER.info("Hello World");
+	}
+
+}
